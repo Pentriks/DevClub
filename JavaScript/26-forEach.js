afdffb2279d67fc6a3fe4ae93e5304f3913d27eb
Endrik <endriksan555@gmail.com>
2026-05-02 14:41:12 -0300

    aulaForEach

---
+++ JavaScript/26-forEach.js
@@ -0,0 +1,10 @@
+let users = [
+    {name: 'Pentriks', age: 23, contact: '(11) 97280-8111'},
+    {name: 'Luigy', age: 16, contact: '(11) 97280-8110'},
+    {name: 'Pentriks', age: 23, contact: '(11) 97280-8011'},
+    {name: 'Pentriks', age: 23, contact: '(11) 97280-8101'},
+]
+
+users.forEach((item) => {
+    console.log(`Nome: ${item.name}, Idade: ${item.age}, Contato: ${item.contact}`)   
+});
