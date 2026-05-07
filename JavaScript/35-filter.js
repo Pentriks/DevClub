e01a456461b594c84459540e80dab0acb985a1b7
Endrik <endriksan555@gmail.com>
2026-05-07 12:42:33 -0300

    aula filter and practices

---
+++ JavaScript/35-filter.js
@@ -0,0 +1,8 @@
+const list = [20, 3, 234, 12, 17, 541, 6, 87, 275, 1000]
+
+const newList = list.filter(number => {
+    const condition = (number < 100) ? true : false
+    return condition
+})
+
+console.log(newList)
