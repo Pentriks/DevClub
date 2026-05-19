6bde89756f4e26d55cae586f838e05f390c0fbcd
Endrik <endriksan555@gmail.com>
2026-05-19 16:09:58 -0300

    modificações nos arquivos for in, for of

--- JavaScript/23-forIn.js
+++ JavaScript/23-forIn.js
@@ -3,3 +3,5 @@ const users = { name: "Pentriks", age: "22", street: "Rua Boa Viagem" }
 for (const key in users) {
     console.log(`${key} : ${users[key]}`)
 }
+// users.name = Pentriks
+// users['name'] = Pentriks
