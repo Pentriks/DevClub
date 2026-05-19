92ecdc9276316eb0b9f426c1380c5eea3d5d523d
luigysantana-dev <luigysantana749@gmail.com>
2026-05-19 16:12:56 -0300

    server

--- nodeJS/conceitos-node/server.js
+++ nodeJS/conceitos-node/server.js
@@ -1,5 +1,22 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
-app.listen(3000)
+app.get('/usuarios/:idProduto', (req, res) => {
+    console.log(req)
+
+    res.send("Rota get acessada com sucesso!")
+})
+
+app.post('/usuarios', (req, res) => {
+    console.log(req)
+    res.send("Rota post acessada!")
+})
+
+// req = requisição
+// res = resposta
+
+app.listen(3001, () => {
+    console.log ("Servidor funcionando")
+})
