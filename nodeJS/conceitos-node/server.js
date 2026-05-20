c9a3fb7b662c36e464bba2edcc90f34f382e1ab9
Endrik <endriksan555@gmail.com>
2026-05-20 16:34:25 -0300

    Executando CRUD

--- nodeJS/conceitos-node/server.js
+++ nodeJS/conceitos-node/server.js
@@ -12,11 +12,38 @@ const users = []
 
 const prisma = new PrismaClient()
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+
+    const users = await prisma.user.findMany()
+
     res.status(200).json(users)
 })
 
-app.post('/usuarios', (req, res) => {
-    users.push(req.body)
-    res.status(201).json({ message: "Cadastro realizado com sucesso!"})
+app.put('/usuarios/:id', async (req, res) => {
+
+   const user = await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email
+        }
+    })
+
+    res.status(200).json(user)
+})
+
+
+
+app.delete('/usuarios/:id', async (req, res) => {
+
+   await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({ message: "Usuário deletado com sucesso!"})
 })
