aa396e4037f797751c14e361513e4752c20d0d72
Endrik <endriksan555@gmail.com>
2026-05-22 13:00:43 -0300

    JavaScript - Modules

---
+++ JavaScript Avançado/modules/index.js
@@ -0,0 +1,5 @@
+const objectAndFunction = require('./objectAndFunctions')
+
+console.log(objectAndFunction.multi(3, 5))
+console.log(objectAndFunction.sum(2, 5))
+console.log(objectAndFunction.person)
