1257884161355280e904962d5175e349cb494d30
Endrik <endriksan555@gmail.com>
2026-05-23 20:26:23 -0300

    JavaScript - this

--- nodeJS/conceitos-node/server.js
+++ nodeJS/conceitos-node/server.js
@@ -4,13 +4,36 @@ import { PrismaClient } from "./generated/prisma/client.js"
 const app = express()
 app.use(express.json())
 
-app.listen(3002, () => {
+app.listen(3003, () => {
     console.log("Servidor funcionando")
 })
 
-const users = []
-
 const prisma = new PrismaClient()
+
+
+app.post('/usuarios', async (req, res) => {
+
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name: req.body.name,
+                age: req.body.age,
+                email: req.body.email
+            }
+        })
+
+        if (user.age < 18) throw new Error("Only allowed users over 18 years old")
+
+        console.log(user)
+
+        return res.status(202).json({ message: "Cadastro realizado com muito sucesso!" })
+
+    } catch (err) {
+        res.status(400).json({error : err.message})
+    } finally {
+        console.log("Terminou tudo!")
+    }
+})
 
 app.get('/usuarios', async (req, res) => {
 
@@ -21,7 +44,7 @@ app.get('/usuarios', async (req, res) => {
 
 app.put('/usuarios/:id', async (req, res) => {
 
-   const user = await prisma.user.update({
+    const user = await prisma.user.update({
         where: {
             id: req.params.id
         },
@@ -39,11 +62,11 @@ app.put('/usuarios/:id', async (req, res) => {
 
 app.delete('/usuarios/:id', async (req, res) => {
 
-   await prisma.user.delete({
+    await prisma.user.delete({
         where: {
             id: req.params.id
         }
     })
 
-    res.status(200).json({ message: "Usuário deletado com sucesso!"})
+    res.status(200).json({ message: "Usuário deletado com sucesso!" })
 })
