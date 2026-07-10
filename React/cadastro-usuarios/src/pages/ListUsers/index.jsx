ec12478377fb24e8fe3a30b435628e7afabbc0c7
Endrik <endriksan555@gmail.com>
2026-07-10 14:23:33 -0300

    utilizando useEffect para listagem de usuarios após o carregamento da pagina

--- React/cadastro-usuarios/src/pages/ListUsers/index.jsx
+++ React/cadastro-usuarios/src/pages/ListUsers/index.jsx
@@ -1,7 +1,20 @@
+import api from '../../services/api'
+import { useEffect } from 'react'
 import Button from '../../components/Button'
 import MainBackground from '../../components/TopBackground'
 
 function ListUsers(){
+
+    useEffect(() => { 
+        
+        async function getUsers(){
+            const usersFromApi = await api.get('/usuarios')
+            console.log(usersFromApi)
+        }
+
+        getUsers()
+       
+    }, [])
 
     return(
 
